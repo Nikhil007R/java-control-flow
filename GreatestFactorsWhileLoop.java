a87516035c038de529af6913c4eb4ba15fada630
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-03-01 11:51:16 +0530

    [Nikhil] Add: find the greatest factor of a number besides itself using a while loop:

---
+++ GreatestFactorsWhileLoop.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class GreatestFactorWhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+
+        if (number <= 1) {
+            System.out.println("No greatest factor exists for " + number);
+        } else {
+            int greatestFactor = 1;
+            int counter = number - 1;
+
+            while (counter >= 1) {
+                if (number % counter == 0) {
+                    greatestFactor = counter;
+                    break;
+                }
+                counter--;
+            }
+
+            System.out.println("The greatest factor of " + number + " besides itself is: " + greatestFactor);
+        }
+    }
+}
