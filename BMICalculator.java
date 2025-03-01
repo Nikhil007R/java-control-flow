60299baa3276f1cc71a2cf6928c461f9ea289831
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-03-01 13:40:37 +0530

    [Nikhil] Add: to calculate the BMI (Body Mass Index) and determine the weight status

---
+++ BMICalculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BMICalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter weight (kg): ");
+        double weight = sc.nextDouble();
+        
+        System.out.print("Enter height (cm): ");
+        double heightCm = sc.nextDouble();
+        
+        double heightM = heightCm / 100;
+        double bmi = weight / (heightM * heightM);
+        
+        System.out.printf("BMI: %.2f\n", bmi);
+        
+        if (bmi <= 18) {
+            System.out.println("Weight Status: Underweight");
+        } else if (bmi > 18 && bmi <= 24.9) {
+            System.out.println("Weight Status: Normal");
+        } else if (bmi >= 25 && bmi <= 39.9) {
+            System.out.println("Weight Status: Overweight");
+        } else {
+            System.out.println("Weight Status: Obese");
+        }
+    }
+}
