60a4aa1bc109bd94c05fe935a30cecd1df23e1b3
Nikhil Rawat <nikhilrawaa311@gmail.com>
2025-03-01 11:16:26 +0530

    [Nikhil] Add: factorial using while loop

---
+++ FactorialWhileLoop.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FactorialWhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+
+        if (number < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else {
+            long factorial = 1;
+            int i = number;
+
+            while (i > 1) {
+                factorial *= i;
+                i--;
+            }
+
+            System.out.println("The factorial of " + number + " is " + factorial);
+        }
+    }
+}
